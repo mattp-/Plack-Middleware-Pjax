35aad2b705a5a3b9b8e652c7a8da5cd33eff9d66
Matt Phillips <mattp@cpan.org>
2011-12-04 17:15:41 -0500

    first drop of working middleware

--- lib/Plack/Middleware/Pjax.pm
+++ lib/Plack/Middleware/Pjax.pm
@@ -5,9 +5,8 @@ package Plack::Middleware::Pjax;
 use parent qw/Plack::Middleware/;
 
 use Plack::Util;
-use HTML::Marpa;
-
-use Data::Printer;
+use Plack::Request;
+use Marpa::HTML;
 
 sub call {
     my ($self, $env) = @_;
@@ -15,37 +14,38 @@ sub call {
     my $res = $self->app->($env);
 
     Plack::Util::response_cb($res, sub {
+        my $req = Plack::Request->new($env);
         my $res = shift;
 
-        my $h = Plack::Util::headers($res->[1]);
-        return unless defined $h->get('HTTP_X_PJAX');
+        return unless defined $req->header('HTTP_X_PJAX');
 
-        #my $tag = $h->get('HTTP_X_PJAX_CONTAINER') || 'data-pjax-container';
-        my $tag = 'data-pjax-container';
+        my $tag = $req->header('HTTP_X_PJAX_CONTAINER') || 'data-pjax-container';
 
         my $body = [];
         Plack::Util::foreach($res->[2], sub { push @$body, $_[0]; });
         $body = join '', @$body;
 
-        my ($instance, $slice) = @{
-            html( \$body, {
-                q{title} => sub {
-                    Marpa::HTML::original()
-                },
-                $tag => sub {
-                    $Marpa::HTML::INSTANCE->{pjax}++;
-                    Marpa::HTML::contents();
-                },
-                q{*} => sub { undef },
-                ':TOP' => sub {
-                    [ $Marpa::HTML::INSTANCE, \( join q{}, @{ Marpa::HTML::values() } ) ]
-                },
-            });
-        };
+        my $stripped = Marpa::HTML::html( \$body,{
+            q{*} => sub {
+                if (Marpa::HTML::attributes()->{$tag}) {
+                    push @{$Marpa::HTML::INSTANCE->{stripped}}, Marpa::HTML::contents();
+                }
+            },
+            q{title} => sub {
+                unshift @{$Marpa::HTML::INSTANCE->{stripped}}, Marpa::HTML::original();
+            },
+            ':TOP' => sub {
+                $Marpa::HTML::INSTANCE->{stripped} || [];
+            }
+        });
 
-        if ($instance->{pjax}) {
-            $res->[2] = [ $body ];
-            $h->set('Content-Length', Plack::Util::content_length([ $body ]);
+        # if length is 0 or 1, there was no pjax container in the body,
+        #  so just return the original
+        if (@$stripped > 0) {
+            $res->[2] = [join '', @$stripped];
+            my $h = Plack::Util::headers($res->[1]);
+            # recalculate content length after changing it
+            $h->set('Content-Length', Plack::Util::content_length($res->[2]));
         }
 
         return $res;
